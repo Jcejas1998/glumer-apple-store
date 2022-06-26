4dce5e5b1001f9f767d24679237647196a07f7be
unknown <cejasjoaquin1998@gmail.com>
2022-06-26 15:43:39 -0300

    first commit

--- src/components/Button/Login.jsx
+++ src/components/Button/Login.jsx
@@ -6,85 +6,86 @@ const Login = () => {
       {/* <!-- Button trigger modal --> */}
       <button
         type="button"
-        className="btn btn-outline-primary ms-auto"
+        class="btn btn-outline-primary"
         data-bs-toggle="modal"
         data-bs-target="#exampleModal"
       >
-        <span className="fa fa-sign-in me-1"></span> Login
+        Login
       </button>
 
       {/* <!-- Modal --> */}
       <div
-        className="modal fade"
+        class="modal fade"
         id="exampleModal"
         tabindex="-1"
-        aria-labelledby="loginModalLabel"
+        aria-labelledby="exampleModalLabel"
         aria-hidden="true"
       >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="loginModalLabel">
-                Login
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Modal title
               </h5>
               <button
                 type="button"
-                className="btn-close"
+                class="btn-close"
                 data-bs-dismiss="modal"
                 aria-label="Close"
               ></button>
             </div>
-            <div className="modal-body">
-              <button className="btn btn-primary w-100 mb-4">
-                <span className="fa fa-google me-2">Sing in With Google</span>
-              </button>
-              <button className="btn btn-primary w-100 mb-4">
-                <span className="fa fa-facebook me-2">
-                  Sing in With Facebook
-                </span>
-              </button>
+            <div class="modal-body">
               <form>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputEmail1" className="form-label">
+                <div class="mb-3">
+                  <label for="exampleInputEmail1" class="form-label">
                     Email address
                   </label>
                   <input
                     type="email"
-                    className="form-control"
+                    class="form-control"
                     id="exampleInputEmail1"
                     aria-describedby="emailHelp"
                   />
-                  <div id="emailHelp" className="form-text">
+                  <div id="emailHelp" class="form-text">
                     We'll never share your email with anyone else.
                   </div>
                 </div>
-                <div className="mb-3">
-                  <label htmlFor="exampleInputPassword1" className="form-label">
+                <div class="mb-3">
+                  <label for="exampleInputPassword1" class="form-label">
                     Password
                   </label>
                   <input
                     type="password"
-                    className="form-control"
+                    class="form-control"
                     id="exampleInputPassword1"
                   />
                 </div>
-                <div className="mb-3 form-check">
+                <div class="mb-3 form-check">
                   <input
                     type="checkbox"
-                    className="form-check-input"
+                    class="form-check-input"
                     id="exampleCheck1"
                   />
-                  <label className="form-check-label" htmlFor="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">
                     Check me out
                   </label>
                 </div>
-                <button
-                  type="submit"
-                  className="btn btn-outline-primary w-100 mt-5"
-                >
+                <button type="submit" class="btn btn-primary">
                   Submit
                 </button>
               </form>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button type="button" class="btn btn-primary">
+                Save changes
+              </button>
             </div>
           </div>
         </div>
