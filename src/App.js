fe42fe45cb35bdd233d4c119128c3c911b6305be
unknown <cejasjoaquin1998@gmail.com>
2022-06-26 15:29:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Product from "./components/Product/Product";
+import Contact from "./components/Contactt/Contact";
+import { Routes, Route } from "react-router-dom";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
+import Cart from "./components/Cart/Cart";
+import Checkout from "./components/Checkout/Checkout";
+import Login from "./components/Button/Login";
+import Singup from "./components/Button/Singup";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<Product />} />
+        <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/checkout" element={<Checkout />} />
+        <Route exact path="/products/:id" element={<ProductDetails />} />
+        <Route exact path="/About" element={<About />} />
+        <Route exact path="/Contact" element={<Contact />} />
+        <Route exact path="/Singup" element={<Singup />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/data.jsx
@@ -0,0 +1,91 @@
+const DATA = [
+    {
+        id:0,
+        title : 'Apple iPhone 11 (128 GB) - Negro ',
+        price : 174619,
+        desc : 'Graba videos 4K y captura retratos espectaculares y paisajes increíbles con el sistema de dos cámaras. Toma grandes fotos con poca luz gracias al modo Noche. Disfruta colores reales en las fotos, videos y juegos con la pantalla Liquid Retina de 6.1 pulgadas (3). Aprovecha un rendimiento sin precedentes en los juegos, la realidad aumentada y la fotografía con el chip A13 Bionic. Haz mucho más sin necesidad de volver a cargar el teléfono gracias a su batería de larga duración (2). Y no te preocupes si se moja, el iPhone 11 tiene una resistencia al agua de hasta 30 minutos a una profundidad máxima de 2 metros (1).',
+        img : '/assets/products/iphone-11.webp'
+    },
+    {
+        id:1,
+        title : 'apple-watch-series',
+        price : 
+        85299,
+        desc : 'El Apple Watch Series 7 tiene la pantalla Retina siempre activa más grande y avanzada hasta ahora. Todo se ve increíble y es más fácil de usar. También es el más resistente a golpes gracias a su nuevo cristal frontal. Sus funcionalidades avanzadas te permiten medir tu nivel de oxígeno en la sangre(1), monitorear tus horas de sueño y practicar la conciencia plena. Y tiene un montón de entrenamientos para mantenerte en movimiento, incluyendo Tai Chi y Pilates.',
+        img : '/assets/products/apple-watch-series.webp'
+    },
+    {
+        id:2,
+        title : 'NOTEBOOK APPLE MACBOOK AIR 13 / MGND3LE/A APPLE',
+        price : 200000
+        ,
+        desc : 'La notebook más delgada y ligera de Apple viene con los superpoderes del chip M1. Termina todos tus proyectos mucho más rápido con el CPU de 8 núcleos y disfruta como nunca antes de apps y juegos con gráficos avanzados gracias al GPU de hasta 8 núcleos. Además, el Neural Engine de 16 núcleos se encarga de acelerar todos los procesos de aprendizaje automático. Todo en un diseño silencioso sin ventilador que te ofrece la mayor duración de batería en una MacBook Air: hasta 18 horas. (1) Portátil como siempre, más poderosa que nunca.',
+        img : '/assets/products/macbook-air.jpg'
+    },
+    {
+        id:3,
+        title : 'Apple iPhone 13 (128 GB) - Azul',
+        price : 
+        251774,
+        desc : 'iPhone 13.El sistema de dos cámaras más avanzado en un iPhone. El superrápido chip A15 Bionic. Un gran salto en duración de batería. Un diseño resistente. Y una pantalla Super Retina XDR más brillante.',
+        img : '/assets/products/iphone-13.webp'
+    },
+    {
+        id:4,
+        title : 'Cable Cargador Usb Original Lightning iPhone 6 7 8 X 11 12 + ',
+        price : 1499,
+        desc : 'CABLE LIGHTNING USB 1MT.Compatible con todos los modelos iPhone a partir del iPhone 5.',
+        img : '/assets/products/cargador.webp'
+    },
+    {
+        id:5,
+        title : 'Apple iPad (9ª generación) 10.2" Wi-Fi 64GB - Gris espacial',
+        price : 68899,
+        desc : 'Lleno de potencia, muy fácil de usar y versátil. El nuevo iPad viene con una espectacular pantalla Retina de 10.2 pulgadas, el potente chip A13 Bionic y una cámara frontal ultra gran angular con Encuadre Centrado. Además, es compatible con el Apple Pencil y el Smart Keyboard.(1)Con el iPad, harás de todo como si nada. Y por menos de lo que imaginas.',
+        img : '/assets/products/ipad.webp'
+    },
+    {
+        id:6,
+        title : 'Apple iPhone 13 Pro Max (256 GB) - Plata',
+        price : 371999,
+        desc : 'iPhone 13 Pro Max. El mayor avance en el sistema de cámaras Pro hasta ahora. Pantalla Super Retina XDR con ProMotion que brinda una respuesta más rápida y fluida. Chip A15 Bionic para un rendimiento fuera de serie. Diseño resistente y la mayor duración de batería jamás vista en un iPhone.',
+        img : '/assets/products/iphone-13-max.webp'
+    },
+    {
+        id:7,
+        title : 'Apple iPhone 12 (64 GB) - Blanco',
+        price : 199999,
+        desc : 'El iPhone 12 tiene una espectacular pantalla Super Retina XDR de 6.1 pulgadas (1). Un frente de Ceramic Shield, cuatro veces más resistente a las caídas (2). Modo Noche en todas las cámaras, para que puedas tomar fotos increíbles con poca luz. Grabación, edición y reproducción de video en Dolby Vision con calidad cinematográfica. Y el potente chip A14 Bionic. Además, es compatible con los nuevos accesorios MagSafe, que se acoplan fácilmente a tu iPhone y permiten una carga inalámbrica más rápida (3). Que comience la diversión.',
+        img : '/assets/products/iphone-12.webp'
+    },
+    {
+        id:8,
+        title : 'Apple iPhone SE (3ª generación, 128 GB) - Azul medianoche',
+        price : 164899,
+        desc : 'Chip A15 Bionic superrápido. Una increíble duración de batería y una cámara que es una superestrella. Y además, el vidrio más resistente en un smartphone y botón de inicio con la seguridad de Touch ID.',
+        img : '/assets/products/apple-iphone-se.webp'
+    },
+    {
+        id:9,
+        title : 'Apple MacBook Pro (14 pulgadas, Chip M1 Pro de Apple con CPU de 8 núcleos,, 16 GB RAM, 512 GB SSD) - color plata        ',
+        price : 439990,
+        desc : 'La nueva MacBook Pro ofrece a los usuarios más pro un rendimiento revolucionario. Elige entre el chip M1 Pro o el aún más potente M1 Max para resolver las tareas profesionales más exigentes con una excepcional duración de la batería(1). Además, la MacBook Pro trae una espectacular pantalla Liquid Retina XDR de 14 pulgadas y puertos avanzados para sacarle más provecho que nunca(2).',
+        img : '/assets/products/macbookpro.webp'
+    },
+    {
+        id:10,
+        title : 'Cargador Macbook Pro Air Original Apple Magsafe 1        ',
+        price : 11987,
+        desc : 'Cargador con conector Magsafe1 En Caja Original Sellada.-45W 14,5 V / 3,1 mAh,-60W 16,5 V / 3,65 mAh,-85W 16.5-18.5V / 4,6 mAh',
+        img : '/assets/products/mac-cargador.webp'
+    },
+    {
+        id:11,
+        title : 'Teclado bluetooth Apple Magic QWERTY español España color plata y blanco        ',
+        price : 21000        ,
+        desc : 'Este teclado Apple es el mejor complemento para hacer todo tipo de actividades en tu Mac, ya sea en tu trabajo o en la comodidad de tu hogar. Su diseño innovador y ultra delgado posibilita escribir con mayor rapidez y precisión.',
+        img : '/assets/products/teclado.webp'
+    }
+]
+
+export default DATA
